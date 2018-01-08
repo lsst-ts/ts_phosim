8dcfe186c342d7f46d337fcef88dc7aeb5e1750e
Te-Wei Tsai <twtsaia@gmail.com>
2018-01-08 16:30:47 -0700

    Update the code of testStarAndSubSys.py to use the facade class

--- testStarAndSubSys.py
+++ testStarAndSubSys.py
@@ -14,6 +14,31 @@ def main():
 
 if __name__ == "__main__":
 
+    # PhoSim directory
+    phosimDir = "/Users/Wolf/Documents/bitbucket/phosim_syseng2"
+
+    # Subsystem data direction
+    camDataDir = "./data/camera"
+    M1M3dataDir = "./data/M1M3"
+    M2dataDir = "./data/M2"
+
+    outputDir = "./output"
+    outputImgDir = "./outputImg"
+    cmdSettingFile = "./data/cmdFile/starDefault.cmd"
+    instSettingFile = "./data/instFile/starDefault.inst"
+
+    configFilePath = "./data/telescopeConfig/GT.inst"
+
+    # Set the settings
+    iSim = 6
+    obsId = 9006000
+    aFilter = "g"
+    wavelengthInNm = 500
+    zAngleInDeg = 27.0912
+    rotAngInDeg = -1.2323/np.pi*180.0
+    boresight = (0, 0)
+    mjdTime = 59552.3
+
     # Instantiate the subsystems
     cam = CamSim()
     M1M3 = M1M3Sim()
@@ -24,46 +49,45 @@ if __name__ == "__main__":
     # Instantiate the telescope
     tele = TeleFacade(cam=cam, M1M3=M1M3, M2=M2, phoSimCommu=phoSimCommu)
 
-    # Subsystem data direction
-    camDataDir = "./data/camera"
-    M1M3dataDir = "./data/M1M3"
-    M2dataDir = "./data/M2"
-    phosimDir = "/Users/Wolf/Documents/bitbucket/phosim_syseng2"
-
     # Set the configuration file path
-    configFilePath = "./data/telescopeConfig/GT.inst"
     tele.setConfigFile(configFilePath)
 
     # Set the subsystem directory
     tele.setSubSysConfigFile(camDataDir=camDataDir, M1M3dataDir=M1M3dataDir, M2dataDir=M2dataDir, 
                              phosimDir=phosimDir)
 
-    # Set the path of perturbation file
-    iSim = 6
-    outputFileDir = "./output"
+    # Update the telescope degree of freedom
+    # dofInUm = np.zeros(50)
+
+    # Camera dx
+    # dofInUm[6] = 1000
+    # tele.setDofInUm(dofInUm)
 
     # Write the perturbation command file
-    pertCmdFilePath = tele.writePertBaseOnConfigFile(outputFileDir, seedNum=iSim, saveResMapFig=True)
+    pertCmdFilePath = tele.writePertBaseOnConfigFile(outputDir, zAngleInDeg=zAngleInDeg, 
+                                rotAngInDeg=rotAngInDeg, seedNum=iSim, saveResMapFig=True)
 
     # Add the star
     skySim.addStarByRaDecInDeg(0, 1.196, 1.176, 17)
+    skySim.addStarByRaDecInDeg(1, 1.196, 1.1793, 16)
 
-    # Write the default star command file
-    cmdFilePath = tele.writeDefaultStarCmdFile(outputFileDir, pertFilePath=pertCmdFilePath)
+    # Write the star physical command file
+    cmdFilePath = tele.writeCmdFile(outputDir, cmdSettingFile=cmdSettingFile, 
+                                    pertFilePath=pertCmdFilePath, cmdFileName="star.cmd")
 
+    # Write the star instance file
+    # Use the camRot=0 temporally. Need to update it latter.
+    instFilePath = tele.writeDefaultStarInstFile(outputDir, skySim, obsId, aFilter, boresight=boresight, 
+                                            camRot=0, mjd=mjdTime, sedName="sed_500.txt", wfSensorOn=True, 
+                                            instSettingFile=instSettingFile, instFileName="star.inst")
 
-    # Write the default star instance file
-    obsId = 9006000
-    aFilter = "g"
-    instFilePath = tele.writeDefaultStarInstFile(outputFileDir, skySim, obsId, aFilter, wfSensorOn=True, 
-                                                    instFileName="star.inst")
+    # Write the accumulated DOF file
+    tele.writeAccDofFile(outputDir)
 
     # Get the argument to run the phosim
-    outputImgFileDir = "./outputImg"
-    logFilePath = os.path.join(outputImgFileDir, "phosimStar.log")
-    argString = tele.getPhoSimArgs(instFilePath, cmdFilePath=cmdFilePath, numPro=2, outputDir=outputImgFileDir, 
-                                e2ADC=0, logFilePath=logFilePath)
-
+    logFilePath = os.path.join(outputImgDir, "phosimStar.log")
+    argString = tele.getPhoSimArgs(instFilePath, cmdFilePath=cmdFilePath, numPro=2, outputDir=outputImgDir, 
+                                    e2ADC=0, logFilePath=logFilePath)
+    
     # Run the phosim
     tele.runPhoSim(argString)
-
