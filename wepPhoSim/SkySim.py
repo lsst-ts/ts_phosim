f784bef22ebdb020c763706c87255f652af26d50
Te-Wei Tsai <twtsaia@gmail.com>
2018-01-10 13:09:15 -0700

    Update the functions in SkySim.py and add the related unit test

--- wepPhoSim/SkySim.py
+++ wepPhoSim/SkySim.py
@@ -1,9 +1,13 @@
-import os
+import os, unittest
 import numpy as np
 
 class SkySim(object):
 
     def __init__(self):
+        """
+        
+        Initiate the SkySim object.
+        """
 
         self.starId = np.array([])
         self.ra = np.array([])
@@ -66,8 +70,32 @@ class SkySim(object):
         self.decl = np.append(self.decl, declInDeg)
         self.mag = np.append(self.mag, mag)
 
-    def addStarByFile(self):
-        pass
+    def resetSky(self):
+        """
+        
+        Reset the sky information and delete all existed stars.
+        """
+        
+        self.__init__()
+
+    def addStarByFile(self, readFilePath, skiprows=1):
+        """
+        
+        Add the star data by reading the file.
+        
+        Arguments:
+            readFilePath {[star]} -- Star data file path.
+        
+        Keyword Arguments:
+            skiprows {int} -- Skip the first "skiprows" lines. (default: {1})
+        """
+        
+        # Read the data
+        data = np.loadtxt(readFilePath, skiprows=skiprows)
+
+        # Add the stars
+        for star in data:
+            self.addStarByRaDecInDeg(star[0], star[1], star[2], star[3])
 
     def addStarByChipPos(self):
         pass
@@ -75,11 +103,40 @@ class SkySim(object):
     def addStarByFocalPlane(self):
         pass
 
+class SkySimTest(unittest.TestCase):
+    
+    """
+    Test functions in SkySim.
+    """
+
+    def setUp(self):
+
+        self.skyFile = os.path.join("..", "data", "sky", "wfsStar.txt")
+
+    def testFunc(self):
+
+        # Instantiate the skySim object
+        skySim = SkySim()
+
+        skySim.setStarRaDecInDeg(np.array([0]), np.array([1]), np.array([2]), 
+                                    np.array([3]))
+        self.assertEqual(len(skySim.starId), 1)
+
+        skySim.addStarByRaDecInDeg(1, 2, 3, 4)
+        self.assertEqual(len(skySim.starId), 2)
+
+        try:
+            skySim.addStarByRaDecInDeg(1, 2, 3, 4)
+        except Exception as ValueError:
+            pass
+
+        skySim.resetSky()
+        self.assertEqual(len(skySim.starId), 0)
+
+        skySim.addStarByFile(self.skyFile)
+        self.assertEqual(len(skySim.starId), 8)
+
 if __name__ == "__main__":
 
-    skySim = SkySim()
-
-    starId = 0
-    raInDeg = 0.1
-    declInDeg = 0.1
-    skySim.addStarByRaDecInDeg(starId, raInDeg, declInDeg)
+    # Do the unit test
+    unittest.main()
