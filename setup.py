9119eacc23bef725fc7a6ae2f7d11fc3f9560da7
Te-Wei Tsai <twtsaia@gmail.com>
2018-12-03 13:16:23 -0700

    Rearange the files.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="WepPhosim",
+    version="1.0.0",
+    author="Te-Wei Tsai",
+    author_email="ttsai@lsst.org",
+    packages=["lsst.ts.phosim"]
+)
