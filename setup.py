a68dd1d1a4dc92295561f43a34afa46c0fb45e5b
Te-Wei Tsai <TTsai@lsst.org>
2019-01-10 12:53:59 -0700

    Update the version number to 1.1.0 in setup.py file.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="WepPhosim",
-    version="1.0.0",
+    version="1.1.0",
     author="Te-Wei Tsai",
     author_email="ttsai@lsst.org",
     packages=["lsst.ts.phosim"]
