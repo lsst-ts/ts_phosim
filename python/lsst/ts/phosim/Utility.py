d036d4eda4b989c7f550948553342b7723949c34
David Thomas <davidthomas5412@gmail.com>
2019-01-24 21:59:58 +0000

    add ObsMetaData helpers to Utility.py

--- python/lsst/ts/phosim/Utility.py
+++ python/lsst/ts/phosim/Utility.py
@@ -1,6 +1,8 @@
 import os
 from enum import Enum
 
+from lsst.sims.utils import ObservationMetaData
+
 import lsst.ts.phosim
 
 
@@ -27,6 +29,101 @@ class SurfaceType(Enum):
     L3B = 11
     FP = 12
     Chip = 13
+
+
+def getWfChips():
+    """Get the wavefront sensors on the focal plane.
+
+    Returns
+    -------
+    list or str
+        The wavefront sensor names.
+    """
+    return [
+        ("R:0,0 S:2,2,A", "R:0,0 S:2,2,B"),
+        ("R:0,4 S:2,0,A", "R:0,4 S:2,0,B"),
+        ("R:4,0 S:0,2,A", "R:4,0 S:0,2,B"),
+        ("R:4,4 S:0,0,A", "R:4,4 S:0,0,B")
+        ]
+
+
+def createObservation(obsId=None, filterType=None, boresight=None,
+                       zAngleInDeg=None, rotAngInDeg=None, mjd=None):
+    """Create a new Observation based on the provided parameters.
+
+    Parameters
+    ----------
+    obsId : int, optional
+        Observation Id. (the default is None.)
+    filterType : FilterType, optional
+        Active filter type. (the default is None.)
+    boresight : tuple, optional
+        Telescope boresight in (ra, decl). (the default is None.)
+    zAngleInDeg : float, optional
+        Zenith angle in degree. (the default is None.)
+    rotAngInDeg : float, optional
+        Camera rotation angle in degree between -90 and 90 degrees. (the
+        default is None.)
+    mjd : float, optional
+        MJD of observation. (the default is None.)
+
+    Returns
+    _______
+    ObservationMetaData
+        An observation with the set parameters.
+    """
+    altitude = None
+    if zAngleInDeg is not None:
+        if not (0 <= zAngleInDeg <= 90):
+            raise ValueError("zAngleInDeg must be between 0 and 90 degrees.")
+        altitude = 90 - zAngleInDeg
+    
+    ra = None
+    dec = None
+    if boresight is not None:
+        ra = boresight[0]
+        dec = boresight[1]
+
+    filterString = None
+    if filterType is not None:
+        filterString = filterType.toString()
+
+    obs = ObservationMetaData(
+            mjd=mjd, 
+            pointingRA=ra,
+            pointingDec=dec,
+            rotSkyPos=rotAngInDeg,
+            bandpassName=filterString
+        )
+    obs.OpsimMetaData = {
+        "obsHistID": obsId,
+        "altitude": altitude
+    }
+    return obs
+
+
+def getOpsimObservation(tract=0, target=0):
+    """Get the Opsim Observation corresponding to the tract and target.
+    The tracts are shown in doc/tracts.png.
+
+    Parameters
+    ----------
+    tract : int, optional
+        The tract to get the catalog from. 
+        (the default is 0. See doc/tracts.png)
+    target : int, optional
+        The target within a tract to get the catalog from. 
+        (the default is 0. See doc/tracts.png)
+
+    Returns
+    -------
+    ObservationMetaData
+        The selected observation.
+    """
+    tractFile = os.path.join(getModulePath(), "configData/tract/{}".format(tract))
+    with open(tractFile, "rb") as r:
+        tract = pickle.load(r, encoding="latin1")
+    return tract[target]
 
 
 def getModulePath(module=lsst.ts.phosim, startIdx=1, endIdx=-4):
