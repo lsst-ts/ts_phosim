b2894046bce246ef4ad770d1f8adce966bea4450
Te-Wei Tsai <TTsai@lsst.org>
2019-05-07 12:40:44 -0700

    Add the eups package files.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="WepPhosim",
-    version="1.1.1",
-    author="Te-Wei Tsai",
-    author_email="ttsai@lsst.org",
-    packages=["lsst.ts.phosim"]
-)
