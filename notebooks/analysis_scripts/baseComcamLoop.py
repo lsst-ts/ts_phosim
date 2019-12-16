ffa8e4df6ba4b285f9cfb1515b0562223822c24f
Bryce Kalmbach <jbkalmbach@gmail.com>
2019-12-16 15:33:47 -0800

    Adding centroid analysis code.

---
+++ notebooks/analysis_scripts/baseComcamLoop.py
@@ -0,0 +1,462 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+import numpy as np
+import shutil
+
+from lsst.ts.wep.ParamReader import ParamReader
+from lsst.ts.wep.Utility import FilterType, CamType, runProgram
+from lsst.ts.wep.ctrlIntf.WEPCalculationFactory import WEPCalculationFactory
+from lsst.ts.wep.ctrlIntf.RawExpData import RawExpData
+
+from lsst.ts.ofc.Utility import InstName
+from lsst.ts.ofc.ctrlIntf.OFCCalculationFactory import OFCCalculationFactory
+
+from lsst.ts.phosim.telescope.TeleFacade import TeleFacade
+from lsst.ts.phosim.PhosimCmpt import PhosimCmpt
+from lsst.ts.phosim.SkySim import SkySim
+from lsst.ts.phosim.Utility import getPhoSimPath, getAoclcOutputPath, \
+                                   getConfigDir
+from lsst.ts.phosim.PlotUtil import plotFwhmOfIters
+
+
+class AnalysisPhosimCmpt(PhosimCmpt):
+
+    def _writeOpdZkFile(self, zkFileName, rotOpdInDeg):
+        """Write the OPD in zk file.
+        OPD: optical path difference.
+        Parameters
+        ----------
+        zkFileName : str
+            OPD in zk file name.
+        rotOpdInDeg : float
+            Rotate OPD in degree in the counter-clockwise direction.
+        """
+
+        testOutputDir = os.environ["closeLoopTestDir"]
+        filePath = os.path.join(testOutputDir, zkFileName)
+        opdData = self._mapOpdToZk(rotOpdInDeg)
+        header = "The followings are OPD in rotation angle of %.2f degree in um from z4 to z22:" % (
+            rotOpdInDeg)
+        np.savetxt(filePath, opdData, header=header)
+
+    def reorderAndSaveWfErrFile(self, listOfWfErr, refSensorNameList,
+                                zkFileName="wfs.zer"):
+        """Reorder the wavefront error in the wavefront error list according to
+        the reference sensor name list and save to a file.
+        The unexisted wavefront error will be a numpy zero array. The unit is
+        um.
+        Parameters
+        ----------
+        listOfWfErr : list [lsst.ts.wep.ctrlIntf.SensorWavefrontData]
+            List of SensorWavefrontData object.
+        refSensorNameList : list
+            Reference sensor name list.
+        zkFileName : str, optional
+            Wavefront error file name. (the default is "wfs.zer".)
+        """
+
+        # Get the sensor name that in the wavefront error map
+        wfErrMap = self._transListOfWfErrToMap(listOfWfErr)
+        nameListInWfErrMap = list(wfErrMap.keys())
+
+        # Reorder the wavefront error map based on the reference sensor name
+        # list.
+        reorderedWfErrMap = dict()
+        for sensorName in refSensorNameList:
+            if sensorName in nameListInWfErrMap:
+                wfErr = wfErrMap[sensorName]
+            else:
+                numOfZk = self.getNumOfZk()
+                wfErr = np.zeros(numOfZk)
+            reorderedWfErrMap[sensorName] = wfErr
+
+        # Save the file
+        testOutputDir = os.environ["closeLoopTestDir"]
+        filePath = os.path.join(testOutputDir, zkFileName)
+        wfsData = self._getWfErrValuesAndStackToMatrix(reorderedWfErrMap)
+        header = "The followings are ZK in um from z4 to z22:"
+        np.savetxt(filePath, wfsData, header=header)
+
+class baseComcamLoop():
+
+    def main(self, phosimDir, numPro, iterNum, baseOutputDir, 
+            testName, isEimg=False, genOpd=True, genDefocalImg=True, genFlats=True,
+            useMinDofIdx=False, inputSkyFilePath="", m1m3ForceError=0.05):
+
+        # Prepare the calibration products (only for the amplifier images)
+        sensorNameList = self._getComCamSensorNameList()
+        if ((not isEimg) & (genFlats is True)):
+            fakeFlatDir = self._makeCalibs(baseOutputDir, sensorNameList)
+
+        # Make the ISR directory
+        isrDirName = "input"
+        isrDir = os.path.join(baseOutputDir, isrDirName)
+        if genFlats is True:
+            self._makeDir(isrDir)
+
+        # Test star magnitude
+        starMag = 15
+
+        # Survey parameters
+        surveySettingFilePath = os.path.join(getConfigDir(),
+                                            "surveySettings.yaml")
+        surveySettings = ParamReader(filePath=surveySettingFilePath)
+        filterType = FilterType.fromString(
+            surveySettings.getSetting("filterType"))
+        raInDeg = surveySettings.getSetting("raInDeg")
+        decInDeg = surveySettings.getSetting("decInDeg")
+        rotAngInDeg = surveySettings.getSetting("rotAngInDeg")
+
+        # Prepare the components
+        phosimCmpt = self._preparePhosimCmpt(phosimDir, filterType, raInDeg, decInDeg,
+                                        rotAngInDeg, numPro, isEimg,
+                                        m1m3ForceError)
+
+        wepCalc = self._prepareWepCalc(isrDir, filterType, raInDeg, decInDeg,
+                                rotAngInDeg, isEimg)
+
+        tele = phosimCmpt.getTele()
+        defocalDisInMm = tele.getDefocalDistInMm()
+        wepCalc.setDefocalDisInMm(defocalDisInMm)
+
+        ofcCalc = self._prepareOfcCalc(filterType, rotAngInDeg)
+
+        # Ingest the calibration products (only for the amplifier images)
+        if ((not isEimg) & (genFlats is True)):
+            wepCalc.ingestCalibs(fakeFlatDir)
+
+        # Only use 10 hexapod positions and first 3 bending modes of M1M3 and M2
+        if (useMinDofIdx):
+            self._useMinDofIdx(ofcCalc)
+
+        # Set the telescope state to be the same as the OFC
+        state0 = ofcCalc.getStateAggregated()
+        phosimCmpt.setDofInUm(state0)
+
+        # Do the iteration
+        obsId = 9006000
+        opdZkFileName = str("opd.zer" + '.' + testName)
+        wfsZkFileName = str("wfs.zer" + '.' + testName)
+        opdPssnFileName = "PSSN.txt"
+        outputDirName = "pert"
+        outputImgDirName = "img"
+        iterDefaultDirName = "iter"
+        dofInUmFileName = "dofPertInNextIter.mat"
+        skyInfoFileName = "skyComCamInfo.txt"
+        for iterCount in range(iterNum):
+
+            # Set the observation Id
+            phosimCmpt.setSurveyParam(obsId=obsId)
+
+            # The iteration directory
+            iterDirName = "%s%d" % (iterDefaultDirName, iterCount)
+
+            # Set the output directory
+            outputDir = os.path.join(baseOutputDir, iterDirName, outputDirName)
+            phosimCmpt.setOutputDir(outputDir)
+
+            # Set the output image directory
+            outputImgDir = os.path.join(baseOutputDir, iterDirName,
+                                        outputImgDirName)
+            phosimCmpt.setOutputImgDir(outputImgDir)
+
+            # Generate the OPD image
+            if genOpd is True:
+                argString = phosimCmpt.getComCamOpdArgsAndFilesForPhoSim()
+                phosimCmpt.runPhoSim(argString)
+
+            # Analyze the OPD data
+            phosimCmpt.analyzeComCamOpdData(zkFileName=opdZkFileName,
+                                            pssnFileName=opdPssnFileName)
+
+            # Get the PSSN from file
+            pssn = phosimCmpt.getOpdPssnFromFile(opdPssnFileName)
+            print("Calculated PSSN is %s." % pssn)
+
+            # Get the GQ effective FWHM from file
+            gqEffFwhm = phosimCmpt.getOpdGqEffFwhmFromFile(opdPssnFileName)
+            print("GQ effective FWHM is %.4f." % gqEffFwhm)
+
+            # Set the FWHM data
+            listOfFWHMSensorData = phosimCmpt.getListOfFwhmSensorData(
+                opdPssnFileName, sensorNameList)
+            ofcCalc.setFWHMSensorDataOfCam(listOfFWHMSensorData)
+
+            # Prepare the faked sky
+            if (inputSkyFilePath == ""):
+                # According to the OPD field positions
+                metr = phosimCmpt.getOpdMetr()
+                skySim = self._prepareSkySim(metr, starMag)
+                print("Use the default OPD field positions to be star positions.")
+                print("The star magnitude is chosen to be %.2f." % starMag)
+            else:
+                skySim = self._prepareSkySimBySkyFile(inputSkyFilePath)
+
+            # Output the sky information
+            skySim, wepCalc = self._outputSkyInfo(outputDir, skyInfoFileName, skySim, wepCalc)
+
+            # Assign the entra- and intra-focal observation Id
+            extraObsId = obsId + 1
+            intraObsId = obsId + 2
+
+            # Generate the defocal images
+            simSeed = 1000
+            argStringList = phosimCmpt.getComCamStarArgsAndFilesForPhoSim(
+                extraObsId, intraObsId, skySim, simSeed=simSeed,
+                cmdSettingFileName="starDefault.cmd",
+                instSettingFileName="starSingleExp.inst")
+            if genDefocalImg is True:
+                for argString in argStringList:
+                    phosimCmpt.runPhoSim(argString)
+
+                # Repackage the images based on the image type
+                if (isEimg):
+                    phosimCmpt.repackageComCamEimgFromPhoSim()
+                else:
+                    phosimCmpt.repackageComCamAmpImgFromPhoSim()
+
+            # Collect the defocal images
+            intraRawExpData = RawExpData()
+            intraRawExpDir = os.path.join(outputImgDir,
+                                        phosimCmpt.getIntraFocalDirName())
+            intraRawExpData.append(intraObsId, 0, intraRawExpDir)
+
+            extraRawExpData = RawExpData()
+            extraRawExpDir = os.path.join(outputImgDir,
+                                        phosimCmpt.getExtraFocalDirName())
+            extraRawExpData.append(extraObsId, 0, extraRawExpDir)
+
+            # Calculate the wavefront error and DOF
+            listOfWfErr = wepCalc.calculateWavefrontErrors(
+                intraRawExpData, extraRawExpData=extraRawExpData)
+            ofcCalc.calculateCorrections(listOfWfErr)
+
+            # Record the wfs error with the same order as OPD for the comparison
+            phosimCmpt.reorderAndSaveWfErrFile(listOfWfErr, sensorNameList,
+                                               zkFileName=wfsZkFileName)
+
+            # Set the new aggregated DOF to phosimCmpt
+            dofInUm = ofcCalc.getStateAggregated()
+            phosimCmpt.setDofInUm(dofInUm)
+
+            # Save the DOF file
+            phosimCmpt.saveDofInUmFileForNextIter(
+                dofInUm, dofInUmFileName=dofInUmFileName)
+
+            # Add the observation ID by 10 for the next iteration
+            obsId += 10
+
+        # Summarize the FWHM
+        pssnFiles = [os.path.join(baseOutputDir, "%s%d" % (iterDefaultDirName, num),
+                    outputImgDirName, opdPssnFileName) for num in range(iterNum)]
+        saveToFilePath = os.path.join(baseOutputDir, "fwhmIters.png")
+        plotFwhmOfIters(pssnFiles, saveToFilePath=saveToFilePath)
+
+    def _outputSkyInfo(self, outputDir, skyInfoFileName, skySim, wepCalc):
+
+        outputSkyInfoFilePath = os.path.join(outputDir, skyInfoFileName)
+        skySim.exportSkyToFile(outputSkyInfoFilePath)
+        wepCalc.setSkyFile(outputSkyInfoFilePath)
+
+        return skySim, wepCalc
+
+    def _getComCamSensorNameList(self):
+
+        sensorNameList = ["R22_S00", "R22_S01", "R22_S02", "R22_S10", "R22_S11",
+                        "R22_S12", "R22_S20", "R22_S21", "R22_S22"]
+        return sensorNameList
+
+
+    def _makeCalibs(self, outputDir, sensorNameList):
+
+        fakeFlatDirName = "fake_flats"
+        fakeFlatDir = os.path.join(outputDir, fakeFlatDirName)
+        self._makeDir(fakeFlatDir)
+
+        detector = " ".join(sensorNameList)
+        self._genFakeFlat(fakeFlatDir, detector)
+
+        return fakeFlatDir
+
+
+    def _makeDir(self, directory):
+
+        if (not os.path.exists(directory)):
+            os.makedirs(directory)
+
+
+    def _genFakeFlat(self, fakeFlatDir, detector):
+
+        currWorkDir = os.getcwd()
+
+        os.chdir(fakeFlatDir)
+        self._makeFakeFlat(detector)
+        os.chdir(currWorkDir)
+
+
+    def _makeFakeFlat(self, detector):
+
+        command = "makeGainImages.py"
+        argstring = "--detector_list %s" % detector
+        runProgram(command, argstring=argstring)
+
+
+    def _preparePhosimCmpt(self, phosimDir, filterType, raInDeg, decInDeg, rotAngInDeg,
+                        numPro, isEimg, m1m3ForceError):
+
+        # Set the Telescope facade class
+        tele = TeleFacade()
+        tele.addSubSys(addCam=True, addM1M3=True, addM2=True)
+        tele.setPhoSimDir(phosimDir)
+
+        # Prepare the phosim component
+        phosimCmpt = AnalysisPhosimCmpt(tele)
+
+        # Set the telescope survey parameters
+        boresight = (raInDeg, decInDeg)
+        zAngleInDeg = 27.0912
+        phosimCmpt.setSurveyParam(filterType=filterType, boresight=boresight,
+                                zAngleInDeg=zAngleInDeg, rotAngInDeg=rotAngInDeg)
+
+        # Update the setting file if needed
+        settingFile = phosimCmpt.getSettingFile()
+        if (numPro > 1):
+            settingFile.updateSetting("numPro", numPro)
+        if isEimg:
+            settingFile.updateSetting("e2ADC", 0)
+
+        # Set the seed number for M1M3 surface
+        seedNum = 6
+        phosimCmpt.setSeedNum(seedNum)
+
+        # Set the M1M3 force error
+        phosimCmpt.setM1M3ForceError(m1m3ForceError)
+
+        return phosimCmpt
+
+
+    def _prepareWepCalc(self, isrDirPath, filterType, raInDeg, decInDeg, rotAngInDeg,
+                        isEimg):
+
+        wepCalc = WEPCalculationFactory.getCalculator(CamType.ComCam, isrDirPath)
+        wepCalc.setFilter(filterType)
+        wepCalc.setBoresight(raInDeg, decInDeg)
+        wepCalc.setRotAng(rotAngInDeg)
+
+        if (isEimg):
+            settingFile = wepCalc.getSettingFile()
+            settingFile.updateSetting("imageType", "eimage")
+
+        return wepCalc
+
+
+    def _prepareOfcCalc(self, filterType, rotAngInDeg):
+
+        ofcCalc = OFCCalculationFactory.getCalculator(InstName.COMCAM)
+        ofcCalc.setFilter(filterType)
+        ofcCalc.setRotAng(rotAngInDeg)
+        ofcCalc.setGainByPSSN()
+
+        return ofcCalc
+
+
+    def _prepareSkySim(self, opdMetr, starMag):
+
+        skySim = SkySim()
+
+        starId = 0
+        raInDegList, declInDegList = opdMetr.getFieldXY()
+        for raInDeg, declInDeg in zip(raInDegList, declInDegList):
+            # It is noted that the field position might be < 0. But it is not the
+            # same case for ra (0 <= ra <= 360).
+            if (raInDeg < 0):
+                raInDeg += 360.0
+            skySim.addStarByRaDecInDeg(starId, raInDeg, declInDeg, starMag)
+            starId += 1
+
+        return skySim
+
+
+    def _prepareSkySimBySkyFile(self, inputSkyFilePath):
+
+        skySim = SkySim()
+
+        absSkyFilePath = os.path.abspath(inputSkyFilePath)
+        skySim.addStarByFile(absSkyFilePath)
+
+        return skySim
+
+
+    def _useMinDofIdx(self, ofcCalc):
+
+        ztaac = ofcCalc.getZtaac()
+
+        m1m3Bend = np.zeros(20, dtype=int)
+        m1m3Bend[0: 3] = 1
+
+        m2Bend = np.zeros(20, dtype=int)
+        m2Bend[0: 3] = 1
+
+        ztaac.setZkAndDofInGroups(m1m3Bend=m1m3Bend, m2Bend=m2Bend)
+
+def _eraseFolderContent(targetDir):
+
+    for theFile in os.listdir(targetDir):
+        filePath = os.path.join(targetDir, theFile)
+        if os.path.isfile(filePath):
+            os.unlink(filePath)
+        elif os.path.isdir(filePath):
+            shutil.rmtree(filePath)
+
+
+if __name__ == "__main__":
+
+    # Set the parser
+    parser = argparse.ArgumentParser(
+        description="Run AOS closed-loop simulation (default is amp files).")
+    parser.add_argument("--numOfProc", type=int, default=1,
+                        help="number of processor to run PhoSim (default: 1)")
+    parser.add_argument("--iterNum", type=int, default=5,
+                        help="number of closed-loop iteration (default: 5)")
+    parser.add_argument("--output", type=str, default="",
+                        help="output directory")
+    parser.add_argument("--testOutput", type=str, default="", help="test output directory")
+    parser.add_argument("--testLabel", type=str, default="1",
+                        help="filename identifier for test files")
+    parser.add_argument('--eimage', default=False, action='store_true',
+                        help='Use the eimage files')
+    parser.add_argument('--minDof', default=False, action='store_true',
+                        help='Use 10 hexapod positions and first 3 bending modes of M1M3 and M2')
+    parser.add_argument("--skyFile", type=str, default="",
+                        help="Star Id, ra, dec, and magnitude")
+    parser.add_argument("--m1m3FErr", type=float, default=0.05,
+                        help="Ratio of M1M3 actuator force error between 0 and 1 (default: 0.05)")
+    parser.add_argument('--clobber', default=False, action='store_true',
+                        help='Delete existing output directory')
+    args = parser.parse_args()
+
+    # Run the simulation
+    phosimDir = getPhoSimPath()
+
+    if (args.output == ""):
+        outputDir = getAoclcOutputPath()
+    else:
+        outputDir = args.output
+
+    os.makedirs(outputDir, exist_ok=True)
+    if (args.clobber == True):
+        _eraseFolderContent(outputDir)
+
+    if (args.testOutput == ""):
+        testOutputDir = os.path.dirname(os.path.realpath(__file__))
+    else:
+        testOutputDir = args.testOutput
+
+    os.environ["closeLoopTestDir"] = testOutputDir
+
+    ccLoop = baseComcamLoop()
+    ccLoop.main(phosimDir, args.numOfProc, args.iterNum, outputDir, args.testLabel,
+                isEimg=args.eimage, useMinDofIdx=args.minDof,
+                inputSkyFilePath=args.skyFile, m1m3ForceError=args.m1m3FErr)
---
+++ notebooks/analysis_scripts/runCentroidAnalysis.py
@@ -0,0 +1,121 @@
+# import sys
+# path_to_comcamCloseLoop = '/astro/store/epyc/projects/lsst_comm/ts_phosim/bin.src/'
+# sys.path.append(path_to_comcamCloseLoop)
+import os
+import argparse
+import numpy as np
+from baseComcamLoop import baseComcamLoop as comcamLoop
+from baseComcamLoop import _eraseFolderContent
+from createPhosimCatalog import createPhosimCatalog
+from lsst.ts.phosim.Utility import getPhoSimPath, getAoclcOutputPath, getConfigDir
+
+class centroidLoop(comcamLoop):
+
+    def __init__(self, centroidInfoFileName):
+
+        self.centroidInfoFileName = centroidInfoFileName
+
+    def _outputSkyInfo(self, outputDir, skyInfoFileName,
+                       skySim, wepCalc):
+
+        outputSkyInfoFilePath = os.path.join(outputDir, skyInfoFileName)
+        skySim.exportSkyToFile(outputSkyInfoFilePath)
+        wepCalc.setSkyFile(self.centroidInfoFileName)
+
+        return skySim, wepCalc
+
+class centroidPhosimCatalog(createPhosimCatalog):
+
+    def __init__(self, centroidOffset):
+
+        self.centroidOffset = centroidOffset
+
+    def _addStarsInField(self, skySim, opdMetr, numStars, starSep, starMagList):
+
+        starId = 0
+        raInDegList, declInDegList = opdMetr.getFieldXY()
+
+        raOffset = np.zeros(numStars)
+        decMin = -1 * starSep * (float(numStars-1) / 2)
+        decMax = -1 * decMin
+        decOffset = np.linspace(decMin, decMax, numStars)
+
+        raOffset += self.centroidOffset[0]
+        decOffset += self.centroidOffset[1]
+
+        for raInDeg, declInDeg in zip(raInDegList, declInDegList):
+            # It is noted that the field position might be < 0. But it is not the
+            # same case for ra (0 <= ra <= 360).
+            # if (raInDeg < 0):
+            #     raInDeg += 360.0
+
+            for num in range(numStars):
+                raPerturbed = raInDeg + raOffset[num]
+                decPerturbed = declInDeg + decOffset[num]
+
+                if raPerturbed < 0:
+                    raPerturbed += 360.0
+
+                skySim.addStarByRaDecInDeg(starId, raPerturbed,
+                                           decPerturbed, starMagList[num])
+                starId += 1
+
+        return skySim
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--testLabel", type=str, default="centroid")
+    parser.add_argument("--testOutput", type=str, default="")
+    parser.add_argument("--skyFile", type=str, default="starCat.txt")
+    parser.add_argument("--centroidFile", type=str, default="perturbStarCat.txt")
+    parser.add_argument("--genOpd", default=True, action='store_false')
+    parser.add_argument("--genDefocalImg", default=True, action='store_false')
+    parser.add_argument("--genFlats", default=True, action='store_false')
+    args = parser.parse_args()
+
+    # Load directory paths
+    phosimDir = getPhoSimPath()
+    outputDir = getAoclcOutputPath()
+    testLabel = args.testLabel
+    skyFilePath = args.skyFile
+    centroidSkyFilePath = args.centroidFile
+
+    if (args.testOutput == ""):
+        testOutputDir = os.path.dirname(os.path.realpath(__file__))
+    else:
+        testOutputDir = args.testOutput
+
+    os.environ["closeLoopTestDir"] = testOutputDir
+
+    for offset in range(-10, -41, -10):
+
+        # Clobber
+        if args.genOpd is True:
+            _eraseFolderContent(outputDir)
+        else:
+            if args.genFlats is True:
+                _eraseFolderContent(os.path.join(outputDir, 'fake_flats'))
+                _eraseFolderContent(os.path.join(outputDir, 'input'))     
+            if args.genDefocalImg is True:
+                _eraseFolderContent(os.path.join(outputDir, 'iter0', 'img', 'intra'))
+                _eraseFolderContent(os.path.join(outputDir, 'iter0', 'img', 'extra'))
+
+        createCat = createPhosimCatalog()
+        createCat.createPhosimCatalog(1, 0, 15, 15,
+                                    skyFilePath)
+
+        rand_state = np.random.RandomState(seed=(100+offset))
+        offset_ang = 90 # rand_state.choice(np.arange(0, 360)) # 0 is y-direction (ra), 90 is x-direction (dec)
+        pixelOffset = np.array([np.cos(np.radians(offset_ang))*offset,
+                                np.sin(np.radians(offset_ang))*offset]) # ra is y direction as set up
+        centroidOffset = (pixelOffset * .2) / 3600 # Convert to degrees
+        createCat = centroidPhosimCatalog(centroidOffset)
+        createCat.createPhosimCatalog(1, 0, 15, 15,
+                                      str(centroidSkyFilePath[:-3]+str(offset) + 'ang' + str(offset_ang) + '.txt'))
+
+        ccLoop = centroidLoop(str(centroidSkyFilePath[:-3]+str(offset) + 'ang' + str(offset_ang) + '.txt'))
+        ccLoop.main(phosimDir, 8, 1, outputDir, str(testLabel + '.' + str(offset) + 'ang'+ str(offset_ang)), 
+                    isEimg=False, genOpd=args.genOpd, genDefocalImg=args.genDefocalImg, 
+                    genFlats=args.genFlats, useMinDofIdx=False,
+                    inputSkyFilePath=skyFilePath, m1m3ForceError=0.05)
