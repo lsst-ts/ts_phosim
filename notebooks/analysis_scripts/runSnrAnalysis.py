75165fa59ab41645a25420080ac89ea834a5d677
Bryce Kalmbach <jbkalmbach@gmail.com>
2020-03-17 19:52:24 -0700

    Updated SNR Analysis Code to run with background turned on.

--- notebooks/analysis_scripts/runSnrAnalysis.py
+++ notebooks/analysis_scripts/runSnrAnalysis.py
@@ -8,6 +8,7 @@ from baseComcamLoop import baseComcamLoop as comcamLoop
 from baseComcamLoop import _eraseFolderContent
 from createPhosimCatalog import createPhosimCatalog
 from lsst.ts.phosim.Utility import getPhoSimPath, getAoclcOutputPath, getConfigDir
+from lsst.ts.wep.Utility import runProgram
 
 
 if __name__ == "__main__":
@@ -31,8 +32,9 @@ if __name__ == "__main__":
     outputDir = getAoclcOutputPath()
     testLabel = args.testLabel
     skyFilePath = args.skyFile
-    genFlats = False
-    genOpd = False
+    genFlats = True
+    genOpd = True
+    genDefocalImg = True
 
     if (args.testOutput == ""):
         testOutputDir = os.path.dirname(os.path.realpath(__file__))
@@ -49,12 +51,14 @@ if __name__ == "__main__":
         comcamCmdSettingsFile = 'star%s.cmd' % args.phosimCmdFileSuffix
 
     save_images = True
+    first_mag = None
 
-    for magVal in np.arange(16.0, 9.9, -0.5):
+    for magVal in np.arange(16.0, 11.1, -0.5):
 
         if save_images is True:
             dir_name = 'test_output/mag_quickbg_%.2f_output' % magVal
             if os.path.exists(dir_name):
+                print('Output Directory exists')
                 _eraseFolderContent(dir_name)
             else:
                 os.makedirs(dir_name)
@@ -72,6 +76,43 @@ if __name__ == "__main__":
         #         _eraseFolderContent(os.path.join(outputDir, 'iter0', 'img', 'intra'))
         #         _eraseFolderContent(os.path.join(outputDir, 'iter0', 'img', 'extra'))
 
+        if not genOpd and not genFlats: 
+            print('Copying content of /mag_%s/ to re-use the flats and OPD files...' % first_mag)
+            argString = '-a test_output/mag_quickbg_%s_output/. %s/' % (first_mag, outputDir)
+            runProgram("cp", argstring=argString)
+
+            # ensure that input/raw and input/rerun are empty 
+            print('Deleting content of input/raw/ and input/rerun/')
+            _eraseFolderContent(os.path.join(outputDir, 'input','raw'))
+            _eraseFolderContent(os.path.join(outputDir, 'input','rerun'))
+
+            # remove files that are remade
+            argString = os.path.join(outputDir, 'input')
+            runProgram("rm", argstring=argString+'/isr*')
+            runProgram("rm", argstring=argString+'/registry*')
+            runProgram("rm", argstring=argString+'/_mappe*')
+
+        # Clobber
+        if genOpd is True:
+            print('We will make new OPD files in this run')
+            _eraseFolderContent(outputDir)
+        else:
+            if genFlats is True:
+                print('We will make new flats in this run')
+                _eraseFolderContent(os.path.join(outputDir, 'fake_flats'))
+                _eraseFolderContent(os.path.join(outputDir, 'input'))     
+            if genDefocalImg is True:
+                print('We will make new defocal images in this run ')
+                intraPath = os.path.join(outputDir, 'iter0', 'img', 'intra')
+                extraPath = os.path.join(outputDir, 'iter0', 'img', 'extra')
+                if os.path.exists(intraPath):
+                    _eraseFolderContent(intraPath)
+                if os.path.exists(extraPath):
+                    _eraseFolderContent(extraPath)
+
+        if first_mag is None:
+            first_mag = '%.2f' % magVal
+
         createCat = createPhosimCatalog()
         raShift = (args.raShift * .2) / 3600 # Convert to degrees
         decShift = (args.decShift * .2) / 3600 # Convert to degrees
@@ -81,13 +122,13 @@ if __name__ == "__main__":
         ccLoop = comcamLoop()
         ccLoop.main(phosimDir, args.numOfProc, 1, 
                     outputDir, '%s.%.1f' % (testLabel, magVal), 
-                    isEimg=False, genOpd=True, genDefocalImg=True, 
+                    isEimg=False, genOpd=genOpd, genDefocalImg=True, 
                     genFlats=genFlats, useMinDofIdx=False,
                     inputSkyFilePath=skyFilePath, m1m3ForceError=0.05,
                     opdCmdSettingsFile=opdCmdSettingsFile,
                     comcamCmdSettingsFile=comcamCmdSettingsFile)
 
         # # Once the necessary data is created we don't need to recreate on every iteration
-        # args.opd = False
-        # genFlats = False
+        genFlats = False
+        genOpd = False
         # args.defocalImg = False
