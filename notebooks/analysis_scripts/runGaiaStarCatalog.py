c2a323209e88a0238a023955ed2c5f512f89e4ac
suberlak <suberlak@uw.edu>
2020-05-12 17:17:16 -0700

    added splitWfsByMag capability

--- notebooks/analysis_scripts/runGaiaStarCatalog.py
+++ notebooks/analysis_scripts/runGaiaStarCatalog.py
@@ -9,15 +9,32 @@ from lsst.ts.wep.Utility import runProgram
 # initial setting whether  to calculate opd, etc. 
 flats = False
 opd = False
-defocalImg = True
-justWfs = False # switch to only re-do wfs,  
+defocalImg = False   
 
-selectSensors  = 'wfs' # 'comcam'
+justWfs = True # switch to only re-do wfs,  
+splitWfsByMag = True # whether to calculate the wfs for subsets of stars 
+                     # based on magnitude ranges, or not ...
+                     # the ranges are 11-16 mag, as the normal
+                     # Filter.py g-mag range is 9.74-16.17,
+                     # so even though there are stars in gaia DR2 catalog
+                     # for the region centered on  boresight (ra,dec)=(0,0) 
+                     # from 10 to 22 mag,  
+                     # those fainter  than 16.17 are not used for WFS 
+                     # calculation anyway ... 
+
+
+selectSensors  = 'comcam' #  or 'wfs', or 'None' - if none, then 
+# PhoSim will simulate all sensors that get any brightness on them, 
+# even if it's just one photon - thus if there are any bright stars that 
+# may be saturated, it will simulate these sensors, even if they 
+# are beyond Raft22 , which is a problem  for the phosim_repackager,
+# which is not expecting it ... 
 
 #not making or copying flats, opd, defocalImg...
 
 # just to be consistent...
 if justWfs:
+    print('We are in WFS-calculation only mode, re-using flats, opd, and defocal images ')
     flats=  False
     opd = False
     defocalImg = False # don't re-generate opd
@@ -44,7 +61,7 @@ doDeblending = True
 
 # change the camDimOffset  setting in ts_wep/policy
 # to not select stars that are too close to the edge ...
-camDimOffset = -50
+camDimOffset = -150
 
 
 # we want to save postage stamps - the 
@@ -55,7 +72,7 @@ postageImg = True
 # dir from /analysis_scripts/ level
 # - that's where we save the results:
 topDir = 'results_gaia'
-expDir = 'gMagGt11_R22_' # name of the experiment dir 
+expDir = 'gMagGt11_' # name of the experiment dir 
 
 # dir from /analysis_scripts/ level 
 # - that's where we copy the flats, opd from:
@@ -125,12 +142,11 @@ if not justWfs :
 if selectSensors is 'comcam':
     skyFilePath = os.path.join(topDir,'starCatGAIA_gt11.txt')
 
-
 if selectSensors is 'wfs':
     skyFilePath  = os.path.join(topDir, 'starCatGAIA_gt11_wfs.txt')
  
 
-print('Using sky catalog from %s'%skyFilePath)
+print('Using %s sensors and %s sky catalog'%(selectSensors,skyFilePath))
        
 # set the opd.cmd and star.cmd files ...
 opdCmd  = 'opdQuickBackground.cmd'
@@ -147,5 +163,7 @@ ccLoop.main(phosimDir, numPro, iterNum, outputDir, testLabel,
             doDeblending=doDeblending, camDimOffset = camDimOffset, 
             postageImg=postageImg ,
             opdCmdSettingsFile=opdCmd,
-            comcamCmdSettingsFile=comcamCmd,selectSensors = selectSensors)
+            comcamCmdSettingsFile=comcamCmd, selectSensors = selectSensors,
+            splitWfsByMag =splitWfsByMag
+            )
 print('Done running ccLoop for GAIA \n\n')
