a9431f059298093e9ce450055923bc7d54e86f20
suberlak <suberlak@uw.edu>
2020-06-10 15:06:48 -0700

    cleaned up the code to vary star separation on single amplifier

--- notebooks/analysis_scripts/runSingleAmpStarSep.py
+++ notebooks/analysis_scripts/runSingleAmpStarSep.py
@@ -1,6 +1,3 @@
-# import sys
-# path_to_comcamCloseLoop = '/astro/store/epyc/projects/lsst_comm/ts_phosim/bin.src/'
-# sys.path.append(path_to_comcamCloseLoop)
 import os
 import argparse
 import numpy as np
@@ -10,132 +7,117 @@ from createPhosimCatalogNew import createPhosimCatalog
 from lsst.ts.phosim.Utility import getPhoSimPath, getAoclcOutputPath, getConfigDir
 from lsst.ts.wep.Utility import runProgram
 
-if __name__ == "__main__":
+# Load directory paths
+phosimDir = getPhoSimPath()
+testLabel = 'sep'
 
-    parser = argparse.ArgumentParser()
-    #testOutputDir = '/data/epyc/users/suberlak/Commissioning/aos/aoclc_analysis/'
-    parser.add_argument("--testLabel", type=str, default="sep")
-    parser.add_argument("--testOutput", type=str, default="")
-    parser.add_argument("--skyFile", type=str, default="starCat.txt")
-    parser.add_argument("--raShift", type=float, default=150.0)
-    parser.add_argument("--decShift", type=float, default=150.0)
-    parser.add_argument("--magVal", type=float, default=16)
-    parser.add_argument("--opd", default=True, action='store_false')
-    parser.add_argument("--defocalImg", default=True, action='store_false')
-    parser.add_argument("--flats", default=True, action='store_false')
-    args = parser.parse_args()
+# set the star brightness
+magVal   = 16
 
-    # Load directory paths
-    phosimDir = getPhoSimPath()
-    #outputDir = getAoclcOutputPath()
-    testLabel = args.testLabel
-    #skyFilePath = args.skyFile
-   
+# settings for simulation
+numPro = 20 # number of processors setting in phosimCmptSetting.yaml 
+iterNum  = 1 # number of iterations 
+numFields = 9 # 9 for all CCDs,  3 to get the result quicker... 
 
-    # read the pixel shift off center 
-    raShift = (args.raShift * .02) / 3600 # Convert to degrees
-    decShift = (args.decShift * .02) / 3600 # Convert to degrees
+# We create a PhoSim catalog with 2 stars with magVal brightness,
+# with varying separation 
+numStars  = 2 
+opd = True # initially we calculate opd and flats .. 
+flats = True
+defocalImg = True
 
-    # read the star brightness
-    magVal   = args.magVal
+# since at such small separations the donuts overlap, we need to 
+# turn on the deblending ....
+doDeblending = True 
+
+# do we want to save postage stamps in WepController.py  getDonutMap() ..
+postageImg = True 
+
+# dir from /analysis_scripts/ level... 
+topDir = 'results_after_centroid_update'
+expDir = 'singleAmpSepNew' # name of the experiment dir 
+
+# the opd and wfs are stored here 
+os.environ["closeLoopTestDir"] = os.path.join(topDir, expDir) 
+
+# Run the same OPD simulation for varying star separation ... 
+
+# 
+magList = np.ones(numStars) * magVal
+
+# separation defined as % of  ra amplifier span (eg. 10%, 50%...)
+# we start from largest separation and move closer 
+for starSep in np.arange(1,11)[::-1] : 
+    print('\nStarting ccLoop for separation equal to %d percent of \
+        amplifier ra span '%starSep)
+
+    outputDir = os.path.join(topDir,expDir,'sep_%d' % starSep)
+    print(outputDir)
+    if (not os.path.exists(outputDir)):
+        os.makedirs(outputDir)
+
+    # first time running the loop, opd=True, flats=True,
+    # then it is set to opd=False, flats=False, to
+    # re-use the flats and calibs : copy from the sep_10  ...
+
+    if not opd and not flats : 
+        print('Copying content of /sep_10/ to re-use the flats and OPD files...')
+        argString = '-a '+os.path.join(topDir,expDir,'sep_10')+'/. '+\
+                     outputDir+'/'
+        runProgram("cp", argstring=argString)
+
+        # ensure that input/raw and input/rerun are empty 
+        print('Deleting content of input/raw/ and input/rerun/')
+        _eraseFolderContent(os.path.join(outputDir, 'input','raw'))
+        _eraseFolderContent(os.path.join(outputDir, 'input','rerun'))
+
+        # remove files that are remade
+        argString = os.path.join(outputDir, 'input')
+        runProgram("rm", argstring=argString+'/isr*')
+        runProgram("rm", argstring=argString+'/registry*')
+        runProgram("rm", argstring=argString+'/_mappe*')
+
+    # Clobber everything
+    if opd is True:
+        print('We will make new OPD files in this run')
+        _eraseFolderContent(outputDir)
+
+    else: 
+
+        if flats is True: # Clobber just calibs  
+            print('We will make new flats in this run')
+            _eraseFolderContent(os.path.join(outputDir, 'fake_flats'))
+            _eraseFolderContent(os.path.join(outputDir, 'input'))  
+
+        if defocalImg is True: # Clobber only defocal images 
+            print('We will make new defocal images in this run ')
+            intraPath = os.path.join(outputDir, 'iter0', 'img', 'intra')
+            extraPath = os.path.join(outputDir, 'iter0', 'img', 'extra')
+            if os.path.exists(intraPath):
+                _eraseFolderContent(intraPath)
+            if os.path.exists(extraPath):
+                _eraseFolderContent(extraPath)
 
 
-    # settings for simulation
-    numPro = 80 # number of processors setting in phosimCmptSetting.yaml 
-    iterNum  = 1 # number of iterations 
-    numFields = 9 # 9 for all CCDs,  3 to get the result quicker... 
+    # make the star catalog 
+    skyFilePath = os.path.join(topDir,expDir,'starCat_%d.txt'%starSep)
+    print(skyFilePath)
+    createCat = createPhosimCatalog()
+    createCat.createPhosimCatalog(numStars, starSep, magList,skyFilePath,numFields=numFields,
+                                 addStarsInAmp=True)
 
-    if (args.testOutput == ""):
-        testOutputDir = os.path.dirname(os.path.realpath(__file__))
-    else:
-        testOutputDir = args.testOutput
+    # #initialize the baseComcamLoop.py Class 
+    ccLoop = comcamLoop() 
 
-    os.environ["closeLoopTestDir"] = testOutputDir
- 
+    print('For separation of %d %%,  the outputDir is %s'%(starSep,outputDir))
+    testName = '%s.%d' % (testLabel, starSep)
+    ccLoop.main(phosimDir, numPro, iterNum, outputDir, testName, 
+                isEimg=False,  genOpd=opd, genDefocalImg=defocalImg, 
+                genFlats=flats, useMinDofIdx=False,
+                inputSkyFilePath=skyFilePath, m1m3ForceError=0.05,
+                doDeblending=doDeblending, postageImg=postageImg )
+    print('Done running ccLoop for this separation\n\n')
 
-    # We create a PhoSim catalog with 2 stars with magVal brightness,
-    # with varying separation in degrees     
-    numStars  = 2 
-    args.opd = True # initially we calculate opd and flats .. 
-    args.flats = True
-
-    # since at such small separations the donuts overlap, we need to 
-    # turn on the deblending ....
-    doDeblending = True 
-
-    # we want to save postage stamps..
-    postageImg = True 
-
-    # dir from /analysis_scripts/ level... 
-    topDir = 'results_after_centroid_update'
-    expDir = 'singleAmpSep' # name of the experiment dir 
-    magList = np.ones(numStars) * magVal
-
-    for starSep in np.arange(1,11)[::-1] : 
-        print('\nStarting ccLoop for separation equal to %d percent of \
-            amplifier ra span '%starSep)
-        
-        outputDir = os.path.join(topDir,expDir,'sep_%d' % starSep)
-        print(outputDir)
-        if (not os.path.exists(outputDir)):
-            os.makedirs(outputDir)
-        
-        # re-use the flats and calibs : copy from the sep_10  ...
-
-        if not args.opd and not args.flats : 
-            print('Copying content of /sep_10/ to re-use the flats and OPD files...')
-            argString = '-a '+os.path.join(topDir,expDir,'sep_10')+'/. '+\
-                         outputDir+'/'
-            runProgram("cp", argstring=argString)
-
-            # ensure that input/raw and input/rerun are empty 
-            print('Deleting content of input/raw/ and input/rerun/')
-            _eraseFolderContent(os.path.join(outputDir, 'input','raw'))
-            _eraseFolderContent(os.path.join(outputDir, 'input','rerun'))
-
-            # remove files that are remade
-            argString = os.path.join(outputDir, 'input')
-            runProgram("rm", argstring=argString+'/isr*')
-            runProgram("rm", argstring=argString+'/registry*')
-            runProgram("rm", argstring=argString+'/_mappe*')
-
-        # Clobber
-        if args.opd is True:
-            print('We will make new OPD files in this run')
-            _eraseFolderContent(outputDir)
-        else:
-            if args.flats is True:
-                print('We will make new flats in this run')
-                _eraseFolderContent(os.path.join(outputDir, 'fake_flats'))
-                _eraseFolderContent(os.path.join(outputDir, 'input'))     
-            if args.defocalImg is True:
-                print('We will make new defocal images in this run ')
-                intraPath = os.path.join(outputDir, 'iter0', 'img', 'intra')
-                extraPath = os.path.join(outputDir, 'iter0', 'img', 'extra')
-                if os.path.exists(intraPath):
-                    _eraseFolderContent(intraPath)
-                if os.path.exists(extraPath):
-                    _eraseFolderContent(extraPath)
-        
-       
-        # make the star catalog 
-        skyFilePath = os.path.join(topDir,expDir,'starCat_%d.txt'%starSep)
-        print(skyFilePath)
-        createCat = createPhosimCatalog()
-        createCat.createPhosimCatalog(numStars, starSep, magList,skyFilePath,numFields=numFields)
-        
-        # #initialize the baseComcamLoop.py Class 
-        ccLoop = comcamLoop() 
-        
-        print('For separation of %d %%,  the outputDir is %s'%(starSep,outputDir))
-        ccLoop.main(phosimDir, numPro, iterNum, outputDir, '%s.%d' % (testLabel, starSep), 
-                    isEimg=False,  genOpd=args.opd, genDefocalImg=args.defocalImg, 
-                    genFlats=args.flats, useMinDofIdx=False,
-                    inputSkyFilePath=skyFilePath, m1m3ForceError=0.05,
-                    doDeblending=doDeblending, postageImg=postageImg )
-        print('Done running ccLoop for this separation\n\n')
- 
-        # # Once the necessary data is created we don't need to recreate on every iteration
-        args.opd = False
-        args.flats = False
-        #args.defocalImg = False
+    # # Once the necessary data is created we don't need to recreate on every iteration
+    opd = False
+    flats = False
