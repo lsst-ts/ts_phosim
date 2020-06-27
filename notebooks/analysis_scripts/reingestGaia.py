c6d951d4ddfc46eb07232645cc152af63d010884
suberlak <suberlak@uw.edu>
2020-06-26 20:36:27 -0700

    added argparse to provide args in command line

--- notebooks/analysis_scripts/reingestGaia.py
+++ notebooks/analysis_scripts/reingestGaia.py
@@ -1,27 +1,63 @@
 import os
 import datetime
 import numpy as np
-from reingestCloseLoop import baseReingest  as baseReingest
-from reingestCloseLoop import _eraseFolderContent
-from createPhosimCatalogNew import createPhosimCatalog
-from lsst.ts.phosim.Utility import getPhoSimPath, getAoclcOutputPath, getConfigDir
-from lsst.ts.wep.Utility import runProgram
+import argparse
 from astropy.table import Table
 from astropy.coordinates import SkyCoord
+from reingestCloseLoop import baseReingest  as baseReingest
+from lsst.ts.wep.Utility import runProgram
+
+
+parser = argparse.ArgumentParser(
+    description='Reingest results of running ComCam AOS closed loop for GAIA DR2 catalogs')
+parser.add_argument("--field", type=str, default="",
+                    help='name of GAIA field, eg. high, med, low, Baade. If other, leave empty. ')
+
+parser.add_argument("--inputDir", type=str, default="",
+                    help='input directory' )
+parser.add_argument("--outputDir", type=str, default="",
+                    help='output directory' )
+parser.add_argument("--raInDeg", type=float, default=0, 
+                    help='RA of the field center in degrees')
+parser.add_argument("--decInDeg", type=float, default=0, 
+                    help='DEC of the field center in degrees')
+
+args = parser.parse_args()
 
 
 
-# origin directory  
-field = 'high'
-inputDir = 'results_gaia/dr2_%s_gt11'%field
+# Try setting the origin and destination 
+# directories from the field name 
+field = args.field 
+if len(field)>0:
+    print('Based on provided field name %s, we are assuming the following:'%field)
+    # Set origin directory 
+    inputDir = 'results_gaia/dr2_%s_gt11'%field
 
-# a directory in which we store the rerun
-outputDir = 'results_gaia/dr2_%s_gt11_reingest'%field
+    # a directory in which we store the rerun
+    outputDir = 'results_gaia/dr2_%s_gt11_reingest'%field
+else:
+
+    if (len(args.inputDir)>0) and (len(args.outputDir)>0) : 
+        print("\nReading the inputDir and outputDir")
+        inputDir = 'results_gaia/%s'%args.inputDir
+        outputDir = 'results_gaia/%s'%args.outputDir
+
+        print("Reading the raInDeg, decInDeg for the field center from parser")
+        raInDeg = args.raInDeg
+        decInDeg =args.decInDeg 
+
+    else:
+        print('Need to provide a named field, or full name of inputDir, outputDir')
 
 
+print('inputDir=%s,  outputDir=%s'%(inputDir, outputDir))
+
+# Copy the results of the prior PhoSim run
 if not os.path.exists(outputDir):
-    print('Copying entire content of %s ...'%inputDir)
-    print('This includes calibs, OPD, defocal images, ingest, etc.')
+    print('\nCopying entire content of %s ...'%inputDir)
+    print('This includes calibs, OPD, defocal images, \
+        ingested images, postISR, etc.')
 
     #first copy the old results...
     argString = '-a '+inputDir+'/. '+outputDir+'/'
@@ -39,25 +75,26 @@ print('\nStarting reingest  ')
 print('The outputDir is %s'%outputDir)
 
 
-# set the raInDeg,  decInDeg : 
+if len(args.field)>0:
+    # set the raInDeg,  decInDeg ,
+    # if the field name is provided 
+    # the center of field coords were first defined as 
+    # Galactic: 
+    print('\nCalculating raInDeg, decInDeg based on field name ')
+    gt = Table(data=[['high','med','low','Baade'],
+                                [0,0,0,1.02],
+                               [85,40,10,-3.92 ]], 
+                          names=['name', 'l_deg','b_deg'])
 
-# the center of field coords were first defined as 
-# Galactic: 
+    gaia_coords = SkyCoord(l=gt['l_deg'],b=gt['b_deg'], 
+                           frame='galactic', unit='deg')
+    # convert them to equatorial 
+    gt['ra_deg']= gaia_coords.icrs.ra.deg
+    gt['dec_deg'] = gaia_coords.icrs.dec.deg
 
-gt = Table(data=[['high','med','low','Baade'],
-                            [0,0,0,1.02],
-                           [85,40,10,-3.92 ]], 
-                      names=['name', 'l_deg','b_deg'])
-
-gaia_coords = SkyCoord(l=gt['l_deg'],b=gt['b_deg'], 
-                       frame='galactic', unit='deg')
-# convert them to equatorial 
-gt['ra_deg']= gaia_coords.icrs.ra.deg
-gt['dec_deg'] = gaia_coords.icrs.dec.deg
-
-raInDeg = gt['ra_deg'][gt['name'] == field][0]
-decInDeg = gt['dec_deg'][gt['name'] == field][0]
-print('For this field, the raInDeg=%.3f, decInDeg=%.3f'%(raInDeg,decInDeg))
+    raInDeg = gt['ra_deg'][gt['name'] == field][0]
+    decInDeg = gt['dec_deg'][gt['name'] == field][0]
+    print('For this field, the raInDeg=%.3f, decInDeg=%.3f'%(raInDeg,decInDeg))
 
 
 # initialize the reingestCloseLoop.py Class 
@@ -69,7 +106,7 @@ reingest.main(baseOutputDir = outputDir, genFlats=True,  isEimg=False,
                   )
 print('Done running reingest for %s \n\n'%outputDir)
 
-# move the screenlog generated  by screen -LS  if it exists ... 
+# # move the screenlog generated  by screen -LS  if it exists ... 
 screenlog_path = '/astro/store/epyc/users/suberlak/Commissioning/aos/ts_phosim/notebooks/analysis_scripts'
 screenlog_default = os.path.join(screenlog_path,'screenlog.0')
 
