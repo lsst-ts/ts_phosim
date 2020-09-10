a7e7a75330594fa4baabac9092e0626a3267cf36
suberlak <suberlak@uw.edu>
2020-09-10 12:07:05 -0700

    updated cmdSettings kwargs, printing file content

--- notebooks/analysis_scripts/runGaiaStarCatalog.py
+++ notebooks/analysis_scripts/runGaiaStarCatalog.py
@@ -1,22 +1,31 @@
 import os
 import datetime
 import numpy as np
-from baseComcamLoop import baseComcamLoop as comcamLoop
+from baseComcamLoop import baseComcamLoop
+from baseLsstCamLoop import baseLsstCamLoop
+from baseLsstFamCamLoop import baseLsstFamCamLoop
+
 from baseComcamLoop import _eraseFolderContent
 from createPhosimCatalogNew import createPhosimCatalog
-from lsst.ts.phosim.Utility import getPhoSimPath, getAoclcOutputPath, getConfigDir
+from lsst.ts.phosim.Utility import getPhoSimPath, getAoclcOutputPath, getConfigDir,
 from lsst.ts.wep.Utility import runProgram
-
-# initial setting whether  to calculate opd, etc. 
-flats = False
-opd = False
-defocalImg = True  
-focalImg  = True
-
-copy = True
-
+from astropy.table import Table
+from astropy.coordinates import SkyCoord
 
 justWfs = False # switch to only re-do wfs,  
+
+# initial setting whether  to calculate opd, etc. 
+flats = True
+opd = True
+defocalImg = True  
+focalImg  = False # this is only for comcam  or lsstfamcam 
+
+copy = False
+
+
+
+
+# only for baseComCamLoop... 
 splitWfsByMag = False # whether to calculate the wfs for subsets of stars 
                      # based on magnitude ranges, or not ...
                      # the ranges are 11-16 mag, as the normal
@@ -27,8 +36,8 @@ splitWfsByMag = False # whether to calculate the wfs for subsets of stars
                      # those fainter  than 16.17 are not used for WFS 
                      # calculation anyway ... 
 
-
-selectSensors  = 'comcam' #  or 'wfs', or 'None' - if none, then 
+selectSensors = 'wfs'#'lsstfamcam'#'wfs'# comcam'
+#selectSensors  = 'wfs' #  or 'wfs', or 'None' - if none, then 
 # PhoSim will simulate all sensors that get any brightness on them, 
 # even if it's just one photon - thus if there are any bright stars that 
 # may be saturated, it will simulate these sensors, even if they 
@@ -44,18 +53,19 @@ if justWfs:
     opd = False
     defocalImg = False # don't re-generate opd
     focalImg = False
-    # if just calculating wfs ... 
+    copy = False 
+    # if just re-doing the ts_wep part  ... 
 
 # Load directory paths
 phosimDir = getPhoSimPath()
 testLabel = 'gaia'
 
 # settings for simulation
-numPro = 10 # of 90 : the number of processors to use  in phosimCmptSetting.yaml 
+numPro = 15 # of 90 : the number of processors to use  in phosimCmptSetting.yaml 
             # need to make sure this is many times less than the actual 
             # numProc b/c each process is massively parallel 
 iterNum  = 1 # number of iterations 
-numFields = 9 # 9 for all CCDs,  3 to get the result quicker... 
+numFields = 9 # 9 for all ComCam CCDs,  3 to get the result quicker... 
 
 
 # We use as PhoSim catalog a GAIA star catalog ...  
@@ -64,11 +74,9 @@ numFields = 9 # 9 for all CCDs,  3 to get the result quicker...
 # turn on the deblending ....
 doDeblending = True 
 
-
 # change the camDimOffset  setting in ts_wep/policy
 # to not select stars that are too close to the edge ...
 camDimOffset = -150
-
 
 # we want to save postage stamps - the 
 # default directory is 
@@ -80,18 +88,20 @@ postageImg = True
 topDir = 'results_gaia'
 
 # simulating four galactic locations:  
-field = 'Baade' # 'med' lBaade'
-catalogType = 'gt11' # 'full'
+field = 'med'#'Pleiades' # 'med' lBaade'
+catalogType = 'full' # 'full'
 
-expDir = 'dr2_%s_%s'%(field,catalogType) # name of the experiment dir 
+# to signify we're using 1500 mm in focalplanelayout as opposed to 1000 mm ... 
+expDir = 'dr2_%s_%s_2020_24_wide_%s_1500_noPert'%(field,catalogType,selectSensors) # name of the experiment dir 
+
+
 # 'dr2_med_gt11'
 # 'dr2_low_gt11'
 # 'dr2_baade_gt11'
 
 # dir from /analysis_scripts/ level 
 # - that's where we copy the flats, opd from:
-#copyDir = 'results_before_centroid_update/singleAmpSep/sep_10'
-copyDir = 'results_gaia/dr2_high_gt11'
+copyDir = 'results_gaia/dr2_high_full_2020_24_famcam'
 
 print('\nStarting ccLoop for GAIA catalog')
 
@@ -101,6 +111,19 @@ print('The outputDir is %s'%outputDir)
 # the opd and wfs are stored here 
 os.environ["closeLoopTestDir"] = outputDir
 
+
+# read the star catalog from file ... 
+# it conforms to the format expected by PhoSim 
+# mv /data/epyc/users/suberlak/starCatGAIA.txt analysis_scripts/results_gaia
+if selectSensors is 'wfs':
+    skyFile = 'starCatGAIA_%s_%s_WFS_0.387.txt'%((field,catalogType))
+elif selectSensors is 'comcam':
+    skyFile = 'starCatGAIA_%s_%s.txt'%((field,catalogType))
+elif selectSensors is 'lsstfamcam':
+    skyFile = 'starCatGAIA_%s_%s_LsstFamCam.txt'%((field,catalogType))
+skyFilePath = os.path.join(topDir,skyFile)
+print('Using %s sensors and %s sky catalog'%(selectSensors,skyFilePath))
+       
 
 if (not os.path.exists(outputDir)):
     os.makedirs(outputDir)
@@ -142,10 +165,13 @@ if copy :
     
     # remove files that are remade - this 
     # prevents problems with the registry ... 
-    argString = os.path.join(outputDir, 'input')
-    for remove in ['/isr*','/registry*' ,'/_mappe*']:
-        print('Removing following files from input%s'%remove)
-        runProgram("rm", argstring=argString+remove)
+    inputDirPath = os.path.join(outputDir, 'input')
+    filesToRemove = ['isr_config.py','registry.sqlite3' ,'_mapper']
+    for file in  filesToRemove:
+        filePath = os.path.join(inputDirPath, file)
+        if os.path.exists(filePath):
+            print('Removing following file: input/%s'%file)
+        runProgram("rm", argstring=filePath)
    
     # ensure that input/raw and input/rerun are empty 
     # print('Deleting content of input/raw/ and input/rerun/')
@@ -153,24 +179,26 @@ if copy :
     # _eraseFolderContent(os.path.join(outputDir, 'input','rerun'))
 
 
-    if defocalImg is True:
-        print('We will make new defocal images in this run ')
-        intraPath = os.path.join(outputDir, 'iter0', 'img', 'intra')
-        extraPath = os.path.join(outputDir, 'iter0', 'img', 'extra')
-        pertPath = os.path.join(outputDir, 'iter0','pert')
-        if os.path.exists(intraPath):
-            _eraseFolderContent(intraPath)
-        if os.path.exists(extraPath):
-            _eraseFolderContent(extraPath)
-        if os.path.exists(pertPath):
-            _eraseFolderContent(pertPath)
-
+if defocalImg is True:
+    print('We will make new defocal images in this run ')
+    intraPath = os.path.join(outputDir, 'iter0', 'img', 'intra')
+    extraPath = os.path.join(outputDir, 'iter0', 'img', 'extra')
+    pertPath = os.path.join(outputDir, 'iter0','pert')
+    if os.path.exists(intraPath):
+        _eraseFolderContent(intraPath)
+        print('Erased contents of %s'%intraPath)
+    if os.path.exists(extraPath):
+        _eraseFolderContent(extraPath)
+        print('Erased contents of %s'%extraPath)
+    if os.path.exists(pertPath):
+        _eraseFolderContent(pertPath)
+        print('Erased contents of %s'%pertPath)
 
 
 
 if not opd and not flats and not copy:
     print('Since copy=opd=flats=False, \
-        we expect to see opd and calibs already there ... ') 
+        we expect to see opd and calibs (ingested) already there ... ') 
 
  # only do all that if not trying to just rerun the WFS ... 
 # if not justWfs :  
@@ -239,34 +267,14 @@ if not opd and not flats and not copy:
 #             _eraseFolderContent(extraPath)
 
 
-# read the star catalog from file ... 
-# it conforms to the format expected by PhoSim 
-# mv /data/epyc/users/suberlak/starCatGAIA.txt analysis_scripts/results_gaia
-if selectSensors is 'comcam':
-    skyFile = 'starCatGAIA_%s_%s.txt'%((field,catalogType))
-    skyFilePath = os.path.join(topDir,skyFile)#starCatGAIA_gt11.txt')
-
-# if selectSensors is 'wfs':
-#     skyFilePath  = os.path.join(topDir, 'starCatGAIA_gt11_wfs.txt')
- 
-print('Using %s sensors and %s sky catalog'%(selectSensors,skyFilePath))
-       
-# set the opd.cmd and star.cmd files ...
-opdCmd  = 'opdQuickBackground.cmd'
-comcamCmd = 'starQuickBackground.cmd'
-
-print('For PhoSim using /policy/cmdFile/%s and %s'%(opdCmd,comcamCmd))
 
 # set the raInDeg,  decInDeg : 
 
 # the center of field coords were first defined as 
 # Galactic: 
-from astropy.table import Table
-from astropy.coordinates import SkyCoord
-gt = Table(data=[['high','med','low','Baade'],
-                            [0,0,0,1.02],
-                           [85,40,10,-3.92 ]], 
-                      names=['name', 'l_deg','b_deg'])
+path_to_notebooks = '/astro/store/epyc/users/suberlak/Commissioning/aos/ts_phosim/notebooks/analysis_notebooks/'
+path_to_field_desc = os.path.join(path_to_notebooks,'GAIA_DR2_Galactic_fields.txt' )
+gt = Table.read(path_to_field_desc, format='ascii')
 
 gaia_coords = SkyCoord(l=gt['l_deg'],b=gt['b_deg'], 
                        frame='galactic', unit='deg')
@@ -278,31 +286,89 @@ raInDeg = gt['ra_deg'][gt['name'] == field][0]
 decInDeg = gt['dec_deg'][gt['name'] == field][0]
 print('For this field, the raInDeg=%.3f, decInDeg=%.3f'%(raInDeg,decInDeg))
 
+# set the opd.cmd and star.cmd files ...
+# opdCmd  = 'opdQuickBackground.cmd'
+# starCmd = 'starQuickBackground.cmd'
 
-# initialize the baseComcamLoop.py Class 
-print('Starting baseComcamLoop : ')
-ccLoop = comcamLoop() 
-ccLoop.main(phosimDir, numPro, iterNum, outputDir, testLabel, 
-            isEimg=False,  genOpd=opd, genDefocalImg=defocalImg, genFocalImg=focalImg,
-            genFlats=flats, useMinDofIdx=False,
-            inputSkyFilePath=skyFilePath, m1m3ForceError=0.05,
-            doDeblending=doDeblending, camDimOffset=camDimOffset, 
-            postageImg=postageImg, opdCmdSettingsFile=opdCmd,
-            comcamCmdSettingsFile=comcamCmd, selectSensors=selectSensors,
-            splitWfsByMag =splitWfsByMag,raInDeg=raInDeg,decInDeg=decInDeg
-            )
-print('Done running ccLoop for GAIA \n\n')
+
+opdCmd  = 'opdQuickBackgroundNoPert.cmd'
+starCmd = 'starQuickBackgroundNoPert.cmd'
+
+
+print('For PhoSim using /policy/cmdFile/%s and %s'%(opdCmd,starCmd))
+print('They contain the following:')
+
+phosimPolicyDirAbsPath = getConfigDir()
+for fname in [opdCmd, starCmd]:
+	fpath  = os.path.join(phosimPolicyDirAbsPath, 'cmdFile', fname)
+	print('\n%s'%fpath)
+    print(open(fpath,"r").read()) 
+
+
+if selectSensors is 'comcam':
+
+
+    # initialize the baseComcamLoop.py Class 
+    print('Starting baseComcamLoop : ')
+    ccLoop = baseComcamLoop() 
+    ccLoop.main(phosimDir, numPro, iterNum, outputDir, testLabel, 
+                isEimg=False,  genOpd=opd, genDefocalImg=defocalImg, genFocalImg=focalImg,
+                genFlats=flats, useMinDofIdx=False,
+                inputSkyFilePath=skyFilePath, m1m3ForceError=0.05,
+                doDeblending=doDeblending, camDimOffset=camDimOffset, 
+                postageImg=postageImg, opdCmdSettingsFile=opdCmd,
+                starCmdSettingsFile=starCmd, selectSensors=selectSensors,
+                splitWfsByMag =splitWfsByMag,raInDeg=raInDeg,decInDeg=decInDeg
+                )
+    print('Done running ccLoop for GAIA \n\n')
+
+
+
+elif selectSensors is 'wfs': 
+
+    # initialize the baseWfsWep.py Class 
+    wfsLoop = baseLsstCamLoop() 
+    testName  = '%s' % (testLabel)
+    print('The testName is %s'%testName)
+
+    wfsLoop.main(phosimDir, numPro, iterNum, outputDir, testLabel, 
+                isEimg=False,  genOpd=opd, genDefocalImg=defocalImg, 
+                genFlats=flats, useMinDofIdx=False,
+                inputSkyFilePath=skyFilePath, m1m3ForceError=0.05,
+                doDeblending=doDeblending, camDimOffset=camDimOffset, 
+                postageImg=postageImg, opdCmdSettingsFile=opdCmd, 
+                starCmdSettingsFile=starCmd,raInDeg=raInDeg,decInDeg=decInDeg )
+
+elif selectSensors is 'lsstfamcam':
+
+    # initialize the lsstfamcam.py class 
+    fcLoop = baseLsstFamCamLoop()
+    testName = '%s' % (testLabel)
+    print('The testName is %s'%testName)
+    fcLoop.main(phosimDir, numPro, iterNum, outputDir, testLabel, 
+                genOpd=opd, genDefocalImg=defocalImg, genFocalImg=focalImg,
+                genFlats=flats, inputSkyFilePath=skyFilePath,
+                doDeblending=doDeblending, camDimOffset=camDimOffset, 
+                postageImg=postageImg, opdCmdSettingsFile=opdCmd,
+                starCmdSettingsFile=starCmd, selectSensors=selectSensors,
+                raInDeg=raInDeg,decInDeg=decInDeg
+                )
+    print('Done running lsstFamCam loop for GAIA  \n\n')
+
+
+
+
 
 # move the screenlog generated  by screen -LS  if it exists ... 
 # it should be wherever the screen to run this code 
 # got made ... 
-screenlog_path = '/astro/store/epyc/users/suberlak/Commissioning/aos/ts_phosim/notebooks/analysis_scripts'
-screenlog_default = os.path.join(screenlog_path,'screenlog.0')
+# screenlog_path = '/astro/store/epyc/users/suberlak/Commissioning/aos/ts_phosim/notebooks/analysis_scripts'
+# screenlog_default = os.path.join(screenlog_path,'screenlog.0')
 
-if os.path.exists(screenlog_default):
-    now = datetime.datetime.now()
-    date = now.strftime("%Y-%m-%d_%H:%M:%S")
-    screenlog = 'screenlog_%s_%s_%s.txt'%(topDir,expDir,date)
-    argString = screenlog_default + ' '+outputDir+'/'+screenlog
-    runProgram("mv ", argstring=argString)
-    print('Screenlog saved as %s'%screenlog)
+# if os.path.exists(screenlog_default):
+#     now = datetime.datetime.now()
+#     date = now.strftime("%Y-%m-%d_%H:%M:%S")
+#     screenlog = 'screenlog_%s_%s_%s.txt'%(topDir,expDir,date)
+#     argString = screenlog_default + ' '+outputDir+'/'+screenlog
+#     runProgram("mv ", argstring=argString)
+#     print('Screenlog saved as %s'%screenlog)
