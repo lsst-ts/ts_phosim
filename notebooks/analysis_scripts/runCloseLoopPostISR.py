569bbce62402400cb933d9518617181a40d78d08
suberlak <suberlak@uw.edu>
2020-09-11 13:53:50 -0700

    removing the old db file before proceeding

--- notebooks/analysis_scripts/runCloseLoopPostISR.py
+++ notebooks/analysis_scripts/runCloseLoopPostISR.py
@@ -2,6 +2,8 @@ import os
 import argparse
 import numpy as np
 
+from lsst.utils import getPackageDir
+from lsst.ts.wep.ParamReader import ParamReader
 from lsst.ts.wep.WepController import WepController
 from lsst.ts.wep.Utility import CamType, FilterType, getModulePath, mapFilterRefToG, DefocalType
 from lsst.ts.wep.ctrlIntf.WEPCalculationFactory import WEPCalculationFactory
@@ -12,7 +14,8 @@ import analysisTools as at
 
 def main(closed_loop_input_dir, wfs_zer_output, postage_img_dir,
          save_postage_stamps=True, run_deblender=True,select_sensor = 'comcam', rerun='run1',
-         db_filename = 'bsc1.db3', gaia_field_name='med', rotAngInDeg = 0,
+         db_filename = 'bsc1.db3', gaia_field_name='med', rotAngInDeg = 0, bscDbType='file',
+         setting_filename = 'default.yaml'
           ):
 
     """
@@ -42,7 +45,45 @@ def main(closed_loop_input_dir, wfs_zer_output, postage_img_dir,
 
     gaia_field_name: str, default = '', could be 'low', 'high', 'Baade', 'Pleiades' - corresponds 
         to the field name of GAIA pointing 
+        
+    rotAngInDeg : float, rotation angle in degrees - setting passed to wep_calc
+    
+    bscDbType : str, desired database type, passed on to update the setting file (eg. default.yaml)
+         default='file', could be 'refCat', 'image' 
+    
+    setting_filename: str, name of the setting file for ts_wep/policy/ , eg. 'default.yaml'
+    
     """
+    
+    # update the setting for bscDbType,  if required 
+    # for that we need to manually point to ts_wep
+    # one way to do it is to use lsst.utils.getPackageDir  
+    
+    path_to_ts_wep = getPackageDir("ts_wep")
+    path_to_setting_file = os.path.join(path_to_ts_wep, 'policy',setting_filename)
+    settingFile = ParamReader(filePath=path_to_setting_file)
+    bscDbTypeInFile = settingFile.getSetting("bscDbType")
+    print('%s contains : '%path_to_setting_file, 
+          bscDbTypeInFile)
+    if bscDbTypeInFile != bscDbType:
+        # In the following we update the setting for bscDbType,
+        # saving the change in the default.yaml file 
+        settingFile.updateSetting("bscDbType", bscDbType)
+        settingFile.saveSetting(filePath=path_to_setting_file)
+
+        # check that the change indeed took place 
+        settingFile = ParamReader(filePath=path_to_setting_file)
+        print('After change: ', settingFile.getSetting("bscDbType"))
+    
+    bscDataDir = os.path.join(path_to_ts_wep, 'tests/testData')
+    if db_filename  in os.listdir(bscDataDir):
+        os.remove(os.path.join(bscDataDir,db_filename))
+        print('Removed old %s file'%db_filename)
+    
+    
+    ########################
+    # Initialize wep_calc
+    ########################
     print('select_sensor is %s'%select_sensor)
     if select_sensor == 'comcam':
         wep_calc = WEPCalculationFactory.getCalculator(CamType.ComCam, closed_loop_input_dir)
@@ -124,10 +165,15 @@ if __name__ == '__main__':
                         help="Set tag to save the postage stamps created for each donut.")
     parser.add_argument("--run_deblender", default=True, action='store_true',
                         help="Set tag to use donuts that require deblending")
-    parser.add_argument("--select_sensor", default='comcam', help='Choose sensor family: comcam, lsstcam, or lsstfamcam')
+    parser.add_argument("--select_sensor", default='comcam', 
+                        help='Choose sensor family: comcam, lsstcam, or lsstfamcam')
     parser.add_argument("--rerun", default='run1', help='The postISR rerun dir')
     parser.add_argument("--db_filename", default='bsc1.db', help='The database filename')
     parser.add_argument("--gaia_field_name", default='med', help='The name of GAIA field')
+    parser.add_argument("--bscDbType", default='file',
+                        help='desired database type: refCat, file, or image' )
+    parser.add_argument("--setting_filename", default='default.yaml')
+    parser.add_argument("--rotAngInDeg", default=0, help='rotation angle in degrees')
     args = parser.parse_args()
 
     # detector_list = ['R:2,2 S:0,0', 'R:2,2 S:0,1', 'R:2,2 S:0,2']
@@ -135,4 +181,7 @@ if __name__ == '__main__':
     main(closed_loop_input_dir=args.closed_loop_input_dir, wfs_zer_output=args.wfs_zer_output, 
     postage_img_dir=args.postage_img_dir, save_postage_stamps=args.save_postage_stamps, 
     run_deblender=args.run_deblender,select_sensor = args.select_sensor, rerun=args.rerun,
-    db_filename = args.db_filename, gaia_field_name=args.gaia_field_name)
+    db_filename = args.db_filename, gaia_field_name=args.gaia_field_name,
+        setting_filename=args.setting_filename,
+         bscDbType = args.bscDbType,rotAngInDeg=args.rotAngInDeg
+        )
