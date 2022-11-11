94e688f22a6b637e2b63611f5e016e5be2ca365a
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2022-11-10 17:23:13 -0800

    Added factor of 1/sqrt(2) to conversion.

--- python/lsst/ts/phosim/utils/ConvertZernikesToPsfWidth.py
+++ python/lsst/ts/phosim/utils/ConvertZernikesToPsfWidth.py
@@ -66,19 +66,24 @@ def getPsfGradPerZernike(
         # Create the Zernike polynomial with these coefficients
         Z = galsim.zernike.Zernike(coefs, R_outer=R_outer, R_inner=R_inner)
 
-        # We can calculate the size of the PSF from the RMS of the gradient of
+        # We can calculate the size of the PSF from the RSS of the gradient of
         # the wavefront. The gradient of the wavefront perturbs photon paths.
-        # The RMS quantifies the size of the collective perturbation.
+        # The RSS quantifies the size of the collective perturbation.
         # If we expand the wavefront gradient in another series of Zernike
         # polynomials, we can exploit the orthonormality of the Zernikes to
-        # calculate the RMS from the Zernike coefficients.
-        rms_tilt = np.sqrt(np.sum(Z.gradX.coef**2 + Z.gradY.coef**2))
+        # calculate the RSS from the Zernike coefficients.
+        rss_tilt = np.sqrt(np.sum(Z.gradX.coef**2 + Z.gradY.coef**2))
 
         # Convert to arcsec per micron
-        rms_tilt = np.rad2deg(rms_tilt * 1e-6) * 3600
+        rss_tilt = np.rad2deg(rss_tilt * 1e-6) * 3600
 
         # Convert rms -> fwhm
-        fwhm_tilt = 2 * np.sqrt(2 * np.log(2)) * rms_tilt
+        fwhm_tilt = 2 * np.sqrt(2 * np.log(2)) * rss_tilt
+
+        # Multiply by factor of 1/sqrt(2)
+        # The origin of this factor is uncertain.
+        # It's value is determined empirically from Galsim simulation.
+        fwhm_tilt /= np.sqrt(2)
 
         # Save this conversion factor
         conversion_factors[i] = fwhm_tilt
@@ -97,13 +102,13 @@ def convertZernikesToPsfWidth(
     Parameters
     ----------
     zernikes : np.ndarray
-        Zernike amplitudes (in microns), starting with Noll index 4.
+        Zernike amplitudes (in microns), starting with Noll index `jmin`.
         Either a 1D array of zernike amplitudes, or a 2D array, where each row
         corresponds to a different set of amplitudes.
     jmin : int
         The minimum Zernike Noll index, inclusive. The maximum Noll index is
         inferred from `jmin` and the length of `zernikes`.
-        (the default, 4, ignores piston, x & y offsets, and tilt.)
+        (the default is 4, which ignores piston, x & y offsets, and tilt.)
     R_outer : float
         The outer radius of the telescope aperture, in meters.
         (the default, 4.18, corresponds to the LSST primary mirror.)
@@ -113,7 +118,7 @@ def convertZernikesToPsfWidth(
 
     Returns
     -------
-    np.ndarray
+    dFWHM: np.ndarray
         Quadrature contribution of each Zernike vector to the PSF FWHM
         (in arcseconds).
 
@@ -136,7 +141,19 @@ def convertZernikesToPsfWidth(
     the optimal PSF. In particular, the PSF FWHM will be increased by
     sqrt[(0.1 - 0.1)^2 + (-0.2 - (-0.3))^2 + (0.3 - 0.4)^2] ~ 0.14 arcsecs.
 
-    Note this conversion depends on the aperture of the telescope.
+    This conversion depends on a linear approximation that begins to break down
+    for RSS(dFWHM) > 0.20 arcsecs. Beyond this point, the approximation tends
+    to overestimate the PSF degradation. In other words, if
+    sqrt(sum( dFWHM^2 )) > 0.20 arcsec, it is likely that dFWHM is
+    over-estimated. However, the point beyond which this breakdown begins
+    (and whether the approximation over- or under-estimates dFWHM) can change,
+    depending on which Zernikes have large amplitudes. In general, if you have
+    large Zernike amplitudes, proceed with caution!
+    Note that if the amplitudes Z_est and Z_true are large, this is okay, as
+    long as |Z_est - Z_true| is small.
+
+    For a notebook demonstrating where the approximation breaks down:
+    https://gist.github.com/jfcrenshaw/24056516cfa3ce0237e39507674a43e1
     """
     # Calculate jmax from jmin and the length of the zernike array
     jmax = jmin + zernikes.shape[-1] - 1
@@ -149,5 +166,8 @@ def convertZernikesToPsfWidth(
         R_inner=R_inner,
     )
 
-    # Convert the zernike amplitudes from microns to PSF FWHM
-    return conversion_factors * zernikes
+    # Convert the Zernike amplitudes from microns to their quadrature
+    # contribution to the PSF FWHM
+    dFWHM = conversion_factors * zernikes
+
+    return dFWHM
