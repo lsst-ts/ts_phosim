38115b56d4c9bc23953f9006c342773bd15d21f2
Chris Suberlak <suberlak@uw.edu>
2023-10-11 10:48:37 -0700

    change lsst.ts.wep.utility to utils

--- examples/checkStarCoorWiLsstFAM.py
+++ examples/checkStarCoorWiLsstFAM.py
@@ -22,7 +22,7 @@
 import os
 import numpy as np
 
-from lsst.ts.wep.utility import FilterType, CamType
+from lsst.ts.wep.utils import FilterType, CamType
 
 from lsst.ts.phosim.telescope.TeleFacade import TeleFacade
 from lsst.ts.phosim.SkySim import SkySim
